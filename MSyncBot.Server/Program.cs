05cf7af974807f9a602dfb5691f648e9095915e3
Neelov Eugene <neelov.ev@mail.ru>
2023-12-04 01:04:41 +0300

    Added the minimum main method

--- MSyncBot.Server/Program.cs
+++ MSyncBot.Server/Program.cs
@@ -1,4 +1,23 @@
-﻿static void Main(string[] args)
+﻿using System.Net;
+using MLoggerService;
+
+namespace MSyncBot.Server;
+
+class Program
 {
-    
+    static void Main()
+    {
+        var logger = new MLogger();
+        var server = new Server(IPAddress.Parse("127.0.0.1"), 8888, logger);
+        try
+        {
+            server.Start();
+            logger.LogInformation("Press any key for close program...");
+        }
+        finally
+        {
+            server.Stop();
+        }
+        
+    }
 }
