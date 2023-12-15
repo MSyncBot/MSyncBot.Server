9f2b207a79af3faead35de8e98543adc88925c6d
Neelov Eugene <neelov.ev@mail.ru>
2023-12-16 00:42:10 +0300

    Deleted MSyncBot.Server

--- MSyncBot.Server/Types/MediaFile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MSyncBot.Server.Types;
-
-public class MediaFile(string name, string extension, byte[] data)
-{
-    public string Name { get; set; } = name;
-    public string Extension { get; set; } = extension;
-    public byte[] Data { get; set; } = data;
-}
--- MSyncBot.Server/Types/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MSyncBot.Server.Types;
-
-public class User(string firstName, string? lastName = null, string? username = null)
-{
-    public string FirstName { get; set; } = firstName;
-    public string? LastName { get; set; } = lastName;
-    public string? Username { get; set; } = username;
-    public ulong Id { get; set; }
-}
