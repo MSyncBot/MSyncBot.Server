e8be2102a2531df4cf243bb2bb5288a07c94aa92
Neelov Eugene <neelov.ev@mail.ru>
2023-12-17 13:32:12 +0300

    Added MessageController.cs

---
+++ MSyncBot.API/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSyncBot.API.Types;
+
+namespace MSyncBot.API.Controllers
+{
+    [ApiController]
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+        private static Dictionary<string, List<Message>> messageStore = new();
+
+        // POST api/messages/{socialNetwork}
+        [HttpPost("{socialNetwork}")]
+        public ActionResult<Message> SendMessage(string socialNetwork, Message message)
+        {
+            if (!messageStore.ContainsKey(socialNetwork))
+            {
+                messageStore[socialNetwork] = new List<Message>();
+            }
+
+            messageStore[socialNetwork].Add(message);
+            return Ok(message);
+        }
+
+        // GET api/messages/{socialNetwork}
+        [HttpGet("{socialNetwork}")]
+        public ActionResult<List<Message>> GetMessages(string socialNetwork)
+        {
+            if (messageStore.TryGetValue(socialNetwork, out var value))
+            {
+                return Ok(value);
+            }
+
+            return NotFound();
+        }
+    }
+}
