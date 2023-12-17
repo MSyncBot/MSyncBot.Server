222421c5379f5537379b05eb7d7cb5b1097e3eb2
Neelov Eugene <neelov.ev@mail.ru>
2023-12-17 19:08:18 +0300

    Added Id variable (userId)

--- MSyncBot.Server/Types/User.cs
+++ MSyncBot.Server/Types/User.cs
@@ -1,9 +1,9 @@
 ﻿namespace MSyncBot.Server.Types;
 
-public class User(string firstName, string? lastName = null, string? username = null)
+public class User(string firstName, string? lastName = null, string? username = null, ulong? id = null)
 {
     public string FirstName { get; set; } = firstName;
     public string? LastName { get; set; } = lastName;
     public string? Username { get; set; } = username;
-    public ulong Id { get; set; }
+    public ulong? Id { get; set; } = id;
 }
