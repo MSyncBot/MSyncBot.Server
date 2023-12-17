60b07870fd22c6b4800ee058cc754d950c6a8ba6
Neelov Eugene <neelov.ev@mail.ru>
2023-12-17 19:07:49 +0300

    Added logger

--- MSyncBot.Server/Server.cs
+++ MSyncBot.Server/Server.cs
@@ -5,11 +5,11 @@ using NetCoreServer;
 
 namespace MSyncBot.Server;
 
-class Server(IPAddress address, int port, MLogger logger) : TcpServer(address, port)
+class Server(IPAddress address, int port, MLogger logger) : WsServer(address, port)
 {
-    private MLogger Logger { get; } = logger;
-
-    protected override TcpSession CreateSession() { return new Session(this, Logger); }
+    private MLogger Logger { get; set; } = logger;
+    
+    protected override TcpSession CreateSession() { return new Session(this, logger); }
 
     protected override void OnError(SocketError error)
     {
